2525fb4e1dd41044e3a48407fdc7c5d24f8ddb0e
Toldcat <Andrew.avd@gmail.com>
2020-01-25 18:08:22 +0000

    Get firebase data on startup

--- src/app.js
+++ src/app.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom'
 import AppRouter from './routers/AppRouter'
 import { Provider } from 'react-redux'
 import configureStore from './store/configureStore'
+import { startSetExpenses } from './actions/expenses'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 import './firebase/firebase'
 
 const store = configureStore()
 
-ReactDOM.render(
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>,
-  document.getElementById('app')
-)
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'))
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>,
+    document.getElementById('app')
+  )
+})
